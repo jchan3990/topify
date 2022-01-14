b974b204858cf6dee05be4331ac70cee5a35bda6
Jonathan Chan <jonathan@striim.com>
2022-01-14 01:17:52 -0800

    Added loading page

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="app">
+      <h3>Please wait for authentication</h3>
+    </div>
+  )
+};
+
+export default Loading;
