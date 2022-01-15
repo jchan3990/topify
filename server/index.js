866ff4db785508244d551caacc141b25e57028b7
Jonathan Chan <jonathan@striim.com>
2022-01-15 00:25:50 -0800

    Added / Fixed cookie session

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
-const { authEndpoint, client_id, client_secret } = require("../spotify.js");
+const { authEndpoint, client_id, client_secret, cookie_secret, jwt_secret } = require("../spotify.js");
 const redirect_uri = "http://localhost:3000/auth/callback";
 const fetch = require('node-fetch');
 const express = require("express");
 const Parser = require("body-parser");
 const cors = require('cors');
+const session = require('cookie-session');
+const csurf = require('csurf');
+const jwt = require('jsonwebtoken');
 
 const app = express();
+const router = express.Router();
 const port = 3000;
 
 app.use(Parser.json())
 app.use(Parser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(cors());
+
+app.use(
+  session({
+    name: 'session',
+    keys: [cookie_secret],
+    maxAge: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    secure: false
+  })
+);
+app.use(csurf());
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
@@ -33,7 +47,7 @@ app.get('/auth/login', (req, res) => {
 });
 
 app.get('/auth/callback', async (req, res) => {
-  try {
+
     const { code } = req.query;
     const tokenEndpoint = 'https://accounts.spotify.com/api/token'
     const payload = client_id + ":" + client_secret;
@@ -48,15 +62,16 @@ app.get('/auth/callback', async (req, res) => {
     })
     const data = await response.json();
 
-    const urlParams = new URLSearchParams({
-      access_code: data.access_token,
-      refresh_token: data.refresh_token
-    })
+    const errParams = new URLSearchParams({
+      error: 'invalid_token'
+    });
 
-    return res.redirect('/#' + urlParams.toString());
-  } catch(err) {
-    res.status(400).send(err);
-  }
+    const sessionJWTObject = {
+      token: data.access_token
+    };
+
+    req.session.jwt = jwt.sign(sessionJWTObject, jwt_secret)
+    return res.redirect('/');
 
 });
 
@@ -75,6 +90,19 @@ app.get('/refresh_token', async (req, res) => {
     body: `grant_type=refresh_token&refresh_token=${refresh_token}`
   })
   const data = await response.json();
+
+});
+
+app.get('/auth/current-session', (req, res) => {
+
+  jwt.verify(req.session.jwt, jwt_secret, (err, decodedToken) => {
+    console.log(req.session.jwt)
+    if (err || !decodedToken) {
+      res.send(false);
+    } else {
+      res.send(decodedToken);
+    }
+  });
 
 });
 
