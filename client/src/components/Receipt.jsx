c1342651733bb8eeeb900232247351b8714b4d3b
Jonathan Chan <jonathan@striim.com>
2022-04-28 20:35:55 -0700

    Added Receipt component to show track list

--- client/src/components/Receipt.jsx
+++ client/src/components/Receipt.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 
 import './Receipt.css';
 
-const Receipt = () => {
+const Receipt = ({ tracks }) => {
   return(
     <div id="receipt-container">
-
+      <div className="receipt-paper">
+        <div className="receipt-header">
+          <div style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+            Receipt of Sale
+          </div>
+          <div style={{ fontSize: '30px', fontWeight: 'bold' }}>
+            Topify
+          </div>
+          <div>
+            ------------------------------------------------
+          </div>
+        </div>
+        <div className="receipt-list">
+          <div className="track-list">
+          {tracks.map(track => (
+            <div className="track-item">
+              <span className="track-name" key={track.external_ids.isrc}>{track.name.substring(0, 40) + (track.name.length > 40 ? '...' : '')}</span>
+              <span className="track-amt">{Math.floor(Math.random() * 24 + 1)}</span>
+            </div>
+            ))}
+          </div>
+        </div>
+        <div className="receipt-footer" style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+          THANK YOU!
+        </div>
+      </div>
     </div>
   )
 }
